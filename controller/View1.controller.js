2788d485196a78b2c521c9391e5bcc15daf42601
ra1vi2 <ravishankarverma202@gmail.com>
2020-03-06 11:58:20 +0000

    6March : ra1vi2

--- controller/View1.controller.js
+++ controller/View1.controller.js
@@ -1,25 +1,50 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
-	"sap/m/MessageToast"
-], function (Controller, JSONModel , MessageToast) {
+	"sap/m/MessageToast",
+	"int_test/int_test/util/formatter"
+	//"int_test/util/formatter"
+], function (Controller, JSONModel , MessageToast,formatter) {
 	"use strict";
 
 	return Controller.extend("int_test.int_test.controller.View1", {
 		onInit: function () {
 			//  alert("in initialization");
+			
+			formatter = formatter ; 
+			
+			sap.ui.getCore().getMessageManager().registerObject(this.getView(), true) ;
+			
+			var hierModel = new JSONModel({
+			fName : "Ravi",
+			lName : "Verma",
+			enabled : true,
+			address : {
+				street : "sec",
+				city : "noida",
+				state : "delhi"
+			}
+			});
+			
+		//	var oDataModel = new sap.ui.model.odata.v2.ODataModel();
+		//	
+		//	this.getView().byId("list1").setModel(oDataModel);
+			
+			this.getView().byId("idAddress").setModel(hierModel,"hModel");
+			this.getView().byId("idAdd").setModel(hierModel,"hModel");
 
-			/* var oModel = new JSONModel({
-			 	dynamicText : "This is dynamic Text" ,
+			 var oModel_ = new JSONModel({
+			 	dynamicText : "This is dynamic Label" ,
+			 	enabled : true,
 			 	dynamic2 : "this is second",
 			 	sjdnc: "sdc"
-			 	});*/
-           this.getView().setModel();
+			 	});
+           this.getView().setModel(oModel_,"model");
 			var oModel = this.getOwnerComponent().getModel("testdata");
 			this.getView().byId("idCombo").setModel(oModel, "oModel");
-		    var listModel = this.getOwnerComponent().getModel("listdata");
+		   // var listModel = this.getOwnerComponent().getModel("listdata");
 		
-			this.getView().byId("list1").setModel(listModel, "list");
+		//	this.getView().byId("list1").setModel(listModel, "list");
 		
 		//this.getView().setModel(oModel,"oModel");
 
@@ -39,8 +64,17 @@ sap.ui.define([
 		},
 		
 		onPress: function(){
-			MessageToast.show("Button Clicked");
+		var userid = this.getView().byId("idUserid").getValue().toString();
+		
+		if(userid == "")
+		{
+			this.getView().byId("idUserid").setValueState("Error");
+			MessageToast.show("Invalid User ID");
 		}
-
+		else{
+		
+			MessageToast.show("Login Success!");
+		}
+    }
 	});
 });
